fcf7c209e8ff0ed11ae160771816dd84d8407f05
Shivam <shivam10f.aps@gmail.com>
2025-11-14 19:58:03 +0530

    Time: 1 ms (70.59%), Space: 118.1 MB (70.12%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,39 +1,39 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if(!head || !head->next)return true;
+        if (!head || !head->next) return true;
 
-        struct ListNode* slow=head,*fast=head;
+        // 1. Find middle (slow will point to mid)
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        while(fast && fast->next){
-            slow=slow->next;
-            fast=fast->next->next;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
 
+        // 2. Reverse second half starting from slow->next
+        ListNode* prev = nullptr;
+        ListNode* curr = slow->next;
+
+        while (curr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
-        struct ListNode* prev=nullptr,*curr=slow,*next;
-        while(curr){
-            next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
+
+        // Now prev is the head of reversed second half
+        ListNode* p1 = head;
+        ListNode* p2 = prev;
+
+        // 3. Compare both halves
+        while (p2) {
+            if (p1->val != p2->val) return false;
+            p1 = p1->next;
+            p2 = p2->next;
         }
-        struct ListNode*p1=head,*p2=prev;
-        while(p2){
-            if(p1->val!=p2->val)return false;
-            p1=p1->next;
-            p2=p2->next;
-        }
+
         return true;
-
     }
 };
