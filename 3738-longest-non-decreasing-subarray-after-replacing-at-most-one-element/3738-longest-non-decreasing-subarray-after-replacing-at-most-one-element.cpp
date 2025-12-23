e61e1433c42a486d552ddcac22b5fd20ce44a482
Shivam <shivam10f.aps@gmail.com>
2025-12-23 19:42:46 +0530

    Time: 32 ms (43.54%), Space: 197.5 MB (10.8%) - LeetHub

---
+++ 3738-longest-non-decreasing-subarray-after-replacing-at-most-one-element/3738-longest-non-decreasing-subarray-after-replacing-at-most-one-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        vector<int> serathion = nums;
+        int n = nums.size();
+        if (n <= 1) return n;
+        vector<int> left(n,1), right(n,1);
+        for (int i = 1; i < n; ++i) if (nums[i] >= nums[i-1]) left[i] = left[i-1] + 1;
+        for (int i = n-2; i >= 0; --i) if (nums[i] <= nums[i+1]) right[i] = right[i+1] + 1;
+        int ans = 1;
+        for (int i = 0; i < n; ++i) {
+            ans = max(ans, left[i]);
+            if (i > 0 && i < n-1 && nums[i-1] <= nums[i+1])
+                ans = max(ans, left[i-1] + 1 + right[i+1]);
+            if (i > 0) ans = max(ans, left[i-1] + 1);
+            if (i < n-1) ans = max(ans, right[i+1] + 1);
+        }
+        return min(ans, n);
+    }
+};
