e6b8e2466ba36a9a734973cebd02b2b58dad562b
Shivam kumar <shivam10f.aps@gmail.com>
2026-03-13 16:59:15 +0530

    Time: 5 ms (52.94%), Space: 23.8 MB (80.79%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,21 +4,13 @@ public:
         int n = intervals.size();
         sort(intervals.begin(),intervals.end());
         vector<vector<int>>ans;
-
         for(int i = 0;i<n;i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            if(!ans.empty() && end <= ans.back()[1]) continue;
-
-            for(int j = i+1;j<n;j++){
-                if(end>=intervals[j][0]){
-                    end = max(end,intervals[j][1]);
-                }
+            if(ans.empty() || ans.back()[1] < intervals[i][0]){
+                ans.push_back(intervals[i]);
+            }else{
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
             }
-            ans.push_back({start,end});
         }
         return ans;
-
     }
 };
