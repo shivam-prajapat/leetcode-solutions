2c1c9d9a4c970c460a502bf663ccf5b8b9005c72
Shivam kumar <shivam10f.aps@gmail.com>
2026-04-07 19:58:56 +0530

    Time: 0 ms (100%), Space: 32.7 MB (62.63%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,17 +1,15 @@
-#include <vector>
 
-using namespace std;
 
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        // Exact(goal) = AtMost(goal) - AtMost(goal - 1)
+      
         return atMost(nums, goal) - atMost(nums, goal - 1);
     }
 
 private:
     int atMost(const vector<int>& nums, int goal) {
-        if (goal < 0) return 0; // A sum cannot be less than 0 since the array only has 0s and 1s
+        if (goal < 0) return 0; 
         
         int currentSum = 0;
         int count = 0;
@@ -19,14 +17,13 @@ private:
         
         for (int right = 0; right < nums.size(); ++right) {
             currentSum += nums[right];
-            
-            // Shrink the window from the left if the sum exceeds the goal
+        
             while (currentSum > goal) {
                 currentSum -= nums[left];
                 left++;
             }
             
-            // The number of valid subarrays ending at 'right' is the length of the current window
+            
             count += right - left + 1;
         }
         
