7b5ebde99d3a6636efb48a7fbe78a7fda31537bc
Shivam kumar <shivam10f.aps@gmail.com>
2026-04-17 01:01:19 +0530

    Time: 0 ms (100%), Space: 15.3 MB (17.47%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,35 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        if (root == NULL) return result;
+        vector<vector<int>>result;
 
-        queue<TreeNode*> q;
+        if(root == NULL) return result;
+
+        queue<TreeNode*>q;
         q.push(root);
+        bool check = true;
 
-        bool leftToRight = true;
+        while(!q.empty()){
 
-        while (!q.empty()) {
             int size = q.size();
-            vector<int> level(size);
+            vector<int> row(size);
 
-            for (int i = 0; i < size; i++) {
-                TreeNode* node = q.front();
+            for(int i = 0 ; i<size ; i++){
+                TreeNode*  node = q.front();
                 q.pop();
 
-                // Decide index based on direction
-                int index = leftToRight ? i : size - 1 - i;
-                level[index] = node->val;
+                int index = check ? i : (size - i - 1);
 
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
+                row[index] = node -> val;
+
+                if(node->left){
+                    q.push(node->left);
+                }
+                if(node->right){
+                    q.push(node->right);
+                }
+
+
             }
-
-            result.push_back(level);
-            leftToRight = !leftToRight; // toggle direction
+            check =  !check;
+            result.push_back(row);
         }
-
         return result;
     }
 };
-
