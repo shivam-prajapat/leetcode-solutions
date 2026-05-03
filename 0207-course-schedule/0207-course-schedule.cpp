267a5cc2ca06dbd646eebd64ba45b66f715438cd
Shivam kumar <shivam10f.aps@gmail.com>
2026-05-03 18:47:40 +0530

    Time: 15 ms (10.22%), Space: 20 MB (32.4%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,42 +1,51 @@
 class Solution {
-private:
-      
-    bool DFS(int node,int parent, vector<int>&vis,unordered_map<int,vector<int>>&adj,vector<int>&inRec){
-        vis[node]=1;
-        inRec[node]=1;
-
-        for(auto &it : adj[node]){
-
-            if(!vis[it] && !DFS(it,node,vis,adj,inRec)){return false;
-            } else if(inRec[it]){
-                 return false;
-            }
-           
-        }
-        inRec[node]=0;
-        return true;
-    }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         unordered_map<int,vector<int>>adj;
-        vector<int>inRec(numCourses,0);
+        vector<int>vis(numCourses,0);
+        int count = 0;
 
         for(auto &it : prerequisites){
-            
             int u = it[0];
             int v = it[1];
+
             adj[v].push_back(u);
         }
 
-        vector<int>vis(numCourses,0);
-        
-        for(int i = 0 ;i<numCourses; i++){
-            if(!vis[i] && !DFS(i,-1,vis,adj,inRec)){
-                return false;
+        vector<int>inDegree(numCourses,0);
+
+        for(int i = 0 ;i<numCourses;i++){
+            for(auto &it :adj[i]){
+                inDegree[it]++;
             }
         }
-        return true;
 
-        
+        queue<int>q;
+
+        for(int i = 0 ;i<numCourses; i++){
+            if(inDegree[i]==0){
+                count++;
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int node = q.front();q.pop();
+            
+
+            for(auto &it : adj[node]){
+                inDegree[it]--;
+                if(inDegree[it]==0){
+                     q.push(it);
+                     count++;
+                }
+            }
+        }
+
+        if(count == numCourses ){
+            return true;
+        }else{
+            return false;
+        }
     }
 };
