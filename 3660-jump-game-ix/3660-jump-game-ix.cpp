c68facb555846846017653ab9cdc0e91f0466c50
Shivam kumar <shivam10f.aps@gmail.com>
2026-05-07 19:26:03 +0530

    Time: 8 ms (77.66%), Space: 219.3 MB (85.64%) - LeetHub

---
+++ 3660-jump-game-ix/3660-jump-game-ix.cpp
@@ -0,0 +1,30 @@
+
+
+class Solution {
+public:
+    vector<int> maxValue(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        vector<int> preMax(n, nums[0]);
+    
+        for (int i = 1; i < n; ++i) {
+            preMax[i] = max(preMax[i - 1], nums[i]);
+        }
+        
+        int sufMin = INT_MAX;
+        
+        
+        for (int i = n - 1; i >= 0; --i) {
+            
+            if (preMax[i] > sufMin) {
+                ans[i] = ans[i + 1];
+            } else {
+                ans[i] = preMax[i];
+            }
+          
+            sufMin = min(sufMin, nums[i]);
+        }
+        
+        return ans;
+    }
+};
