47ed0a496b3f3ef186c3e8c6ec9ea88d60bc2aff
Shivam kumar <shivam10f.aps@gmail.com>
2026-06-13 14:51:27 +0530

    Time: 3 ms (70.52%), Space: 18.5 MB (43.47%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,61 +1,48 @@
 class Solution {
 public:
-
-bool hasCycle;  
-
-
-    void DFS(int node, unordered_map<int,vector<int>>&adj, vector<int>&vis, stack<int>&st,vector<int>&inRecursion ) {
-
-        vis[node]=1;
-        inRecursion[node]=1;
-        for(auto &it : adj[node]){
-            if(inRecursion[it]){
-                hasCycle = true;
-                return;
-            }
-
-            if(!vis[it]){
-                DFS(it,adj,vis,st,inRecursion);
-            }
-        }
-        st.push(node);
-        inRecursion[node]=0;
-
-    }
-
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         unordered_map<int,vector<int>>adj;
-        vector<int>vis(numCourses,0);
+
+       
+        vector<int>indeg(numCourses,0);
         vector<int>res;
-        stack<int>st;
-        vector<int>inRecursion(numCourses,0);
 
         for(auto &it : prerequisites){
             int u = it[0];
             int v = it[1];
-
             adj[v].push_back(u);
         }
 
         for(int i = 0 ;i<numCourses;i++){
-            if(!vis[i]){
-                DFS(i,adj,vis,st,inRecursion);
+            for(auto &it : adj[i]){
+                indeg[it]++;
             }
         }
 
-        if(hasCycle){
+        queue<int>q;
+        for(int i = 0 ;i<numCourses; i++){
+            if(indeg[i]==0){
+                    q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            res.push_back(node);
+
+            for(auto &it : adj[node]){
+                indeg[it]--;
+                if(indeg[it]==0)q.push(it);
+            }
+
+        }
+        if(numCourses != res.size()){
             return {};
         }
 
-        while(!st.empty()){
-            res.push_back(st.top());
-            st.pop();
-        }
-
         return res;
-
-
-
-
-    }
+            
+        }
+    
 };
