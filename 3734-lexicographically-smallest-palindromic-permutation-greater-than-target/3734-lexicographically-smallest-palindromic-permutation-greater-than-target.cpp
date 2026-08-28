280e547d51321e052dc392370215fb5bb18aeba2
Shivam kumar <shivam10f.aps@gmail.com>
2026-08-28 21:49:20 +0530

    Time: 3 ms (70.77%), Space: 12.1 MB (67.69%) - LeetHub

--- 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
+++ 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
@@ -1,70 +1,90 @@
 class Solution {
 public:
     string lexPalindromicPermutation(string s, string target) {
-        string calendrix = s;
         int n = s.size();
         vector<int> freq(26, 0);
-        for (char c : s) freq[c - 'a']++;
-        int odd = 0;
-        char mid = 0;
+        for (char c : s) {
+            freq[c - 'a']++;
+        }
+
+        int odd_count = 0;
+        char mid_char = 0;
         for (int i = 0; i < 26; ++i) {
-            if (freq[i] % 2) {
-                odd++;
-                mid = 'a' + i;
+            if (freq[i] % 2 != 0) {
+                odd_count++;
+                mid_char = 'a' + i;
             }
         }
-        if (odd > 1) return "";
-        vector<int> halfCount(26, 0);
-        for (int i = 0; i < 26; ++i) halfCount[i] = freq[i] / 2;
-        int k = n / 2;
-        string prefix = "";
 
-        auto build_from_half = [&](const string &h) {
-            string res = h;
-            if (n % 2 == 1 && mid) res.push_back(mid);
-            res.append(string(h.rbegin(), h.rend()));
+        if (odd_count > 1) return "";
+
+        int h = n / 2;
+        vector<int> half_freq(26, 0);
+        for (int i = 0; i < 26; ++i) {
+            half_freq[i] = freq[i] / 2;
+        }
+
+        auto get_palindrome = [&](const string& left) {
+            string res = left;
+            if (n % 2 != 0) res += mid_char;
+            string rev = left;
+            reverse(rev.begin(), rev.end());
+            res += rev;
             return res;
         };
 
-        auto make_min_half = [&](const vector<int>& hc) {
-            string t;
-            for (int c = 0; c < 26; ++c) if (hc[c]) t.append(hc[c], 'a' + c);
-            return t;
-        };
-        auto make_max_half = [&](const vector<int>& hc) {
-            string t;
-            for (int c = 25; c >= 0; --c) if (hc[c]) t.append(hc[c], 'a' + c);
-            return t;
-        };
-
-        for (int pos = 0; pos < k; ++pos) {
-            bool placed = false;
-            for (int ch = 0; ch < 26; ++ch) {
-                if (halfCount[ch] == 0) continue;
-                halfCount[ch]--;
-                string minRem = make_min_half(halfCount);
-                string maxRem = make_max_half(halfCount);
-
-                string candMin = prefix + char('a' + ch) + minRem;
-                string candMax = prefix + char('a' + ch) + maxRem;
-
-                string fullMin = build_from_half(candMin);
-                string fullMax = build_from_half(candMax);
-
-                if (fullMax <= target) {
-                    halfCount[ch]++; 
-                    continue;
-                }
-
-                prefix.push_back('a' + ch);
-                placed = true;
+        vector<int> cur_freq = half_freq;
+        int max_k = 0;
+        for (int i = 0; i < h; ++i) {
+            int idx = target[i] - 'a';
+            if (cur_freq[idx] > 0) {
+                cur_freq[idx]--;
+                max_k = i + 1;
+            } else {
                 break;
             }
-            if (!placed) return "";
         }
 
-        string finalPal = build_from_half(prefix);
-        if (finalPal > target) return finalPal;
+        if (max_k == h) {
+            string left = target.substr(0, h);
+            string p = get_palindrome(left);
+            if (p > target) {
+                return p;
+            }
+        }
+
+        cur_freq = half_freq;
+        for (int i = 0; i < max_k; ++i) {
+            cur_freq[target[i] - 'a']--;
+        }
+
+        for (int i = max_k; i >= 0; --i) {
+            if (i < max_k) {
+                cur_freq[target[i] - 'a']++;
+            }
+
+            if (i >= h) continue;
+
+            int target_char = target[i] - 'a';
+            for (int c = target_char + 1; c < 26; ++c) {
+                if (cur_freq[c] > 0) {
+                    string left = target.substr(0, i);
+                    left += (char)('a' + c);
+
+                    vector<int> rem_freq = cur_freq;
+                    rem_freq[c]--;
+
+                    for (int r = 0; r < 26; ++r) {
+                        while (rem_freq[r] > 0) {
+                            left += (char)('a' + r);
+                            rem_freq[r]--;
+                        }
+                    }
+
+                    return get_palindrome(left);
+                }
+            }
+        }
 
         return "";
     }
